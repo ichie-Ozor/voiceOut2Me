0281dda6bd24200bb2053b7f33879e610badcbd8
agyanim <agyanimconsultancy@gmail.com>
2023-01-19 10:19:07 +0000

    pages folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Heropage from "./component/Heropage/Heropage";
 // import './App.css';
 
 import { Route, Routes } from "react-router";
+import SoonComingPage from "./pages/SoonComingPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path={"/"}>
-        <Route index element={<Heropage />} />
+        <Route index element={<SoonComingPage />} />
       </Route>
     </Routes>
   );
