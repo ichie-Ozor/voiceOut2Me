c360d71dece20fb0821561f43c59b0b515139b94
agyanim <agyanimconsultancy@gmail.com>
2023-01-19 10:13:05 +0000

    react-router-dom was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-// import Heropage from './component/Heropage/Heropage';
+import Heropage from "./component/Heropage/Heropage";
 // import './App.css';
 
-function App() {
+import { Route, Routes } from "react-router";
+
+const App = () => {
   return (
-    <div className="App text-center text-red-700 text-4xl">
-     {/* <Heropage /> */}
-     <h1>Welcom home</h1>
-    </div>
+    <Routes>
+      <Route path={"/"}>
+        <Route index element={<Heropage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
