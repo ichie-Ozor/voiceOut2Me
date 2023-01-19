0e8be6bde20d359bf4c033439ef5c246c2710cd1
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-01-19 05:52:59 +0000

    tailwind set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Heropage from './component/Heropage/Heropage';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App text-center text-red-700 text-4xl">
+     {/* <Heropage /> */}
+     <h1>Welcom home</h1>
+    </div>
+  );
+}
+
+export default App;
