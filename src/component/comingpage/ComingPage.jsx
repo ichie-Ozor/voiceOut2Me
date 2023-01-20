4f8bb272d09abbf4c5caaaa336e05bab7ee6e1e6
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-01-20 23:40:46 +0000

    hero image resized and animation added

--- src/component/comingpage/ComingPage.jsx
+++ src/component/comingpage/ComingPage.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import NavBar from "./NavBar";
 import HeroImage from "../../assets/images/comingpage/coming-soon-page-hero.svg";
-// import PrimaryBtn1 from "../Buttons/PrimaryBtn1";
+import { Link } from 'react-router-dom'
+import Menu from '../../assets/images/comingpage/menu.svg'
+import Logo from '../../assets/images/comingpage/logo.svg'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
 
 const ComingPage = () => {
   const [email, setEmail] = useState("");
@@ -15,33 +21,45 @@ const ComingPage = () => {
   };
   return (
     <div>
-      <section>
-        <NavBar />
-      </section>
-      <section className=" w-[92vw] md:w-[85vw] m-auto ">
-        <h1 className="font-OpenSand font-normal text-2xl text-center md:text-2xl md:text-left p-2 lg:text-left lg:text-4xl xl:text-5xl mb-2">
+        <header className='w-[90vw] m-auto mb-6 lg:w-[70vw] h-[10vh] '>
+        <section className='flex justify-between  items-center mt-4 bg-[#8155BA] p-2 h-[10vh] '>
+            <div className="logo-title flex justify-center items-center">
+              <div className="image overflow-hidden -ml-4"><img className=' w-24 sm:w-28' src={Logo} alt="logo" /></div>
+              
+              <h1 className='hidden sm:flex  text-white text-xl -ml-5 mt-2'>VoiceOut2Me</h1>
+              </div>
+              <div className="menu overflow-hidden w-8 flex lg:hidden"><img src={Menu} alt="menu" /></div>
+            <Link className="about-us border-[1px] border-white rounded-2xl px-6 py-2 text-lg text-white hover:border-[2px] hidden lg:flex ">About us</Link>
+        </section>
+    </header>
+
+      <section className=" w-[92vw] md:w-[85vw] m-auto lg:mb-16">
+        <h1 className="font-OpenSand font-normal text-2xl text-center md:text-2xl md:text-left p-2 lg:text-center lg:text-3xl xl:text-4xl mb-2">
           Never deal with it alone, seek help and feel better!
         </h1>
-        <p className="font-OpenSand font-normal md:w-[82%] md:text-sm md:text md:ml-[5px] text-center xl:text-2xl lg:text-left lg:text-xl p-2 w-[85%] m-auto">
-          We have ears that are open, so t’s good not to be alright. VoiceOut2Me
+        <p  className="font-OpenSand font-normal md:w-[82%] md:text-sm md:text md:ml-[5px] text-center  lg:text-center lg:text-xl p-2 w-[85%] m-auto lg:ml-14 xl:px-32 xl:text-left"
+        data-aos="fade-up"
+        data-aos-delay="200"
+        >
+          We have ears that are open, so it’s good not to be alright. VoiceOut2Me
           is here to guide you on the path to getting better from depression.
         </p>
       </section>
       <section
-        className="hero-image w-screen h-[15rem] xs:h-[20rem] sm:h-[30rem] lg:h-[40rem] xl:h-[57.938rem] bg-pink-200 mt-[3rem]"
+        className="hero-image w-screen h-[15rem] xs:h-[20rem] sm:h-[30rem] lg:h-[40rem] xl:h-[57.938rem] bg-pink-200 mt-[3rem] lg:w-[60vw] m-auto"
         style={{
           background: `url(${HeroImage})`,
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
         }}
       ></section>
-      <section className="subscribe-wrapper w-[90%] lg:w-[80%] m-auto mt-6 p-4 flex flex-col justify-center items-center gap-8 my-14 max-[320px]:mt-0">
+      <section className="subscribe-wrapper w-[90%] lg:w-[80%] m-auto mt-6 p-4 flex flex-col justify-center items-center gap-8 my-14 max-[320px]:mt-0 lg:-mt-36 xl:-mt-80">
         <h1 className="subscribe-title text-[#290C1E] font-semibold text-lg text-center lg:text-2xl">
           Subscribe to our Newsletter to get full updates when the website
           launches.
         </h1>
         <form className="w-[100%]" action="#" onSubmit={submitHandler}>
-          <div className="input-btn-wrapper flex flex-col gap-2 w-[100%] sm:flex-row items-center justify-center sm:ml-[20%]">
+          <div className="input-btn-wrapper flex flex-col gap-2 w-[100%] sm:flex-row items-center justify-center sm:ml-[20%] ">
             <input
               className="w-[95%] lg:w-[80%] border-[1px] border-[#290C1E] rounded p-2"
               type="text"
@@ -50,12 +68,15 @@ const ComingPage = () => {
               value={email}
             />
             <div className="text-white w-[95%]">
-              {/* <PrimaryBtn1 Name="Notify Me"  /> */}
-              <button className="text-white bg-[#8155BA] w-[100%] sm:w-[30%] py-2 rounded font-bold font-Lato">Notify Me</button>
+              <button className="text-white bg-[#8155BA] w-[100%] sm:w-[30%] py-2 rounded font-bold font-Lato hover:opacity-70">Notify Me</button>
             </div>
           </div>
         </form>
-        <h1 className=" text-lgfont-bold sm:4xl lg:text-4xl text-[#290C1E] font-Lato">We are about to launch!!!</h1>
+        <h1 className=" text-lgfont-bold sm:4xl lg:text-4xl text-[#290C1E] font-Lato"
+                data-aos="fade-up"
+                data-aos-delay="200"
+        
+        >We are about to launch!!!</h1>
       </section>
     </div>
   );
