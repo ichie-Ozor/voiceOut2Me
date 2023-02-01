74d63818448718318426bf048b54b0c3ea898f73
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-02-01 05:33:37 +0000

    burger menu completed

--- src/component/HamburgerMain.jsx
+++ src/component/HamburgerMain.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const HamburgerMain = ({ToggleMenu}) => {
   return (
-    <><div className={classNames(" text-center flex justify-center items-center rounded-full z-40 overflow-hidden ",{"bg-white w-[2.5rem] m-5 md:w-[3rem] md:m-4 mb-10": ToggleMenu})}>
+    <><div className={classNames(" text-center flex justify-center items-center rounded-full z-40 overflow-hidden ",{"bg-white w-[2.5rem] m-5 md:w-[3rem] md:m-4 mb-10  min-[579px]:w-[2.7rem]": ToggleMenu})}>
       <div className={classNames("sm:w-12 w-8 h-8 sm:h-10 flex flex-col gap-[5px] sm:gap-[10px] overflow-hidden border-none mr-3 p-1 lg:hidden z-40",{
-        "pl-2":ToggleMenu
+        "pl-2  min-[579px]:pl-3":ToggleMenu
       })}>
-      <span className={classNames("w-[2.5rem] h-[0.25rem] md:w-[2.5rem] bg-[#8155BA]  rounded", { "w-[1.7rem] h-[0.25rem] rotate-45 origin-[1px]  transition-all duration-[0.3s]": ToggleMenu })}></span>
+      <span className={classNames("w-[2.5rem] h-[0.25rem] md:w-[2.5rem] bg-[#8155BA]  rounded", { "w-[1.7rem] h-[0.25rem] rotate-45 origin-[1px] min-[579px]:origin-[-5.5px] transition-all duration-[0.3s]": ToggleMenu })}></span>
       <span className={classNames("w-[2.5rem] h-[0.25rem] bg-[#8155BA]  rounded", { "w-[1.7rem] h-[0.25rem] scale-0 ": ToggleMenu })}></span>
-      <span className={classNames("w-[2.5rem] h-[0.25rem] md:w-[2.5rem] bg-[#8155BA]  rounded", { "w-[1.7rem] h-[0.25rem] -rotate-45 origin-[1px]  transition-all duration-[0.3s]": ToggleMenu })}></span>
+      <span className={classNames("w-[2.5rem] h-[0.25rem] md:w-[2.5rem] bg-[#8155BA]  rounded", { "w-[1.7rem] h-[0.25rem] -rotate-45 origin-[1px]  min-[579px]:origin-[-5.5px] transition-all duration-[0.3s]": ToggleMenu })}></span>
     </div>
       </div>
     </>
